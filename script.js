652778035e4a710c096bf2241ede9ae66d69cc4a
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-06-24 15:40:03 -0500

    almost working

--- script.js
+++ script.js
@@ -29,17 +29,32 @@ let arrayOfTodos = [
 
   const populateTodos = () => {
     
-    let helpme = document.getElementById("todo-list") 
+    let list = document.getElementById("todo-list")
 
-    for (i=0; 1 < arrayOfTodos.length; i++){
+    for (let i=0; 1 < arrayOfTodos.length; i++){
+    let newLI = document.createElement('lI')
+    let newH1 = document.createElement('H1')
+    let newH2 = document.createElement('H2')
+    let newH3 = document.createElement('H3')
+    let newH4 = document.createElement('H4')
 
-    let poop = document.createElement('li')
+    let userId = document.createTextNode(arrayOfTodos[i].userId)
+    newH1.appendChild(userId)
 
-    let fork = document.createTextNode(arrayOfTodos[i].title)
+    let id = document.createTextNode(arrayOfTodos[i].id)
+    newH2.appendChild(id)
 
-    poop.appendChild(fork)
+    let title = document.createTextNode(arrayOfTodos[i].title)
+    newH3.appendChild(title)
 
-    helpme.appendChild(poop)
+    let completed = document.createTextNode(arrayOfTodos[i].completed)
+    newH4.appendChild(completed)
+
+    newLI.appendChild(newH1)
+    newLI.appendChild(newH2)
+    newLI.appendChild(newH3)
+    newLI.appendChild(newH4)
+    list.appendChild(newLI)
+
     }
-
   }
