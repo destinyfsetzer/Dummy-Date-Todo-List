febdabda66ce2320b25301adca4d1f241260ef3e
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-06-27 18:14:06 -0500

    almost there..

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
-
 let arrayOfTodos = [
     {
     "userId": 14,
@@ -12,63 +10,97 @@ let arrayOfTodos = [
     "id": 2,
     "title": "delectus aut autem",
     "completed": false
-  }]
-
-
-//  console.log(arrayOfTodos[0].userId) // => 14
-//  console.log(arrayOfTodos[1].userId) // => 20
-
-  let fontColor = ""
-
-  const fetchTodos = () => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-    .then( (response) => response.json())
-    .then( (json) => arrayOfTodos = json)
   }
+]
 
-  const logTodos = () => {
-    console.log(arrayOfTodos)
-  }
+let x = ""
 
-  const populateTodos = () => {
-    let list = document.getElementById("todo-list")
+const storeID = (num) => {
+  x = parseInt (num)
+  console.log (num)
+}
 
-    for (i=0; 1 < arrayOfTodos.length; i++){
+let fontColor = ""
 
-        if(arrayOfTodos[i].completed === true){
-        fontColor = "green"
-        } else {
-        fontColor = "red"}
-     
-    let newLi = document.createElement('LI')
-    let newH1 = document.createElement('H1')
-    let newH2 = document.createElement('H2')
-    let newH3 = document.createElement('H3')
-    let newH4 = document.createElement('H4')
+const fetchTodos = () => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+  .then( (response) => response.json())
+  .then( (json) => arrayOfTodos = json)
+}
 
-    let userId = document.createTextNode(arrayOfTodos[i].userId)
-    newH1.appendChild(userId)
+const logTodos = () => {
+  console.log(arrayOfTodos)
+}
 
-    let id = document.createTextNode(arrayOfTodos[i].id)
-    newH2.appendChild(id)
+const populateAllTodos = () => {
+  populateTodos(arrayOfTodos)
+}
 
-    let title = document.createTextNode(arrayOfTodos[i].title)
-    newH3.appendChild(title)
+const populateTodos = (arr) => {
+  let list = document.getElementById("todo-list")
 
-    let completed = document.createTextNode(arrayOfTodos[i].completed)
-    newH4.appendChild(completed)
-    newLi.appendChild(newH1)
-    newLi.appendChild(newH2)
-    newLi.appendChild(newH3)
-    newLi.appendChild(newH4)
-    list.appendChild(newLi)
-
-    newH4.style.color = fontColor
+  for (i=0; i < arr.length; i++){
     
-    }
+  let newLi = document.createElement('LI')
+  let newH1 = document.createElement('H1')
+  let newH2 = document.createElement('H2')
+  let newH3 = document.createElement('H3')
+  let newH4 = document.createElement('H4')
+
+  let userId = document.createTextNode(arr[i].userId)
+  newH1.appendChild(userId)
+
+  let id = document.createTextNode(arr[i].id)
+  newH2.appendChild(id)
+
+  let title = document.createTextNode(arr[i].title)
+  newH3.appendChild(title)
+
+  let completed = document.createTextNode(arr[i].completed)
+  newH4.appendChild(completed)
+
+  newLi.appendChild(newH1)
+  newLi.appendChild(newH2)
+  newLi.appendChild(newH3)
+  newLi.appendChild(newH4)
+  list.appendChild(newLi)
+
+  if(arr[i].completed === true) {
+    fontColor = "green"
+    } else {
+    fontColor = "red"
   }
 
-  
+  newH4.style.color = fontColor
+  }
+}
 
-  //make it red like bloodshed because i'm dying 
+const showUserID = () => {
+  let arrayOfUserTodos = arrayOfTodos.filter(function(numID) {
+    return numID.userId === x
+  })
+  populateTodos(arrayOfUserTodos)
+}
+
+const showCompleted = () => {
+  let arrayOfCompletedTodos = arrayOfTodos.filter(function(todo) {
+    return todo.completed === true
+  })
+  populateTodos(arrayOfCompletedTodos)
+}
+
+const showUnCompleted = () => {
+  let arrayOfUnCompletedTodos = arrayOfTodos.filter(function(todo) {
+    return todo.completed === false
+  })
+  populateTodos(arrayOfUnCompletedTodos)
+}
+
+const clearList = () => {
+  const arrayOfTodos = document.getElementById("todo-list")
+  for ( i = 0; i < arrayOfTodos.length; i++)
+  arrayOfTodos[i].innerHTML=null
+}
+
+
 
